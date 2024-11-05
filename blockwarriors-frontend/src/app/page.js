1c620828df0dcdef2fb01332cc29c12f6e5a51f8
Hao Teng <haoteng10@gmail.com>
2024-11-04 19:19:42 -0500

    feat: supabase modules

--- blockwarriors-frontend/src/app/page.js
+++ blockwarriors-frontend/src/app/page.js
@@ -1,13 +1,16 @@
-import Image from "next/image";
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <main>
       <h1>Block Warriors</h1>
-      <p>We are excited to announce the BlockWarriors AI Challenge, a unique global competition hosted by Princeton's E-Club. This event is designed to bring together the brightest minds in computer science to compete in Minecraft-based PvP minigames. Participants will form teams to design and implement algorithms that control a team of four AI bots.
-
-</p>
+      <p>
+        We are excited to announce the BlockWarriors AI Challenge, a unique
+        global competition hosted by Princeton's E-Club. This event is designed
+        to bring together the brightest minds in computer science to compete in
+        Minecraft-based PvP minigames. Participants will form teams to design
+        and implement algorithms that control a team of four AI bots.
+      </p>
     </main>
-    
   );
 }
