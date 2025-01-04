946aba374f8e068225ae0b8949e317d50eb11dcb
DIodide <ibraheem.amin3@gmail.com>
2025-01-03 19:06:27 -0500

    Revert "Merge branch 'hteng-user-team-registration-v2' of https://github.com/project-blockwarriors/blockwarriors-cmd into token-auth"
    This reverts commit 2f27f15694738c3d5752289046ea70e7771ac605, reversing
    changes made to 1141415ebed520e94e2964d547230b18d25661eb.

--- blockwarriors-frontend/server.js
+++ blockwarriors-frontend/server.js
@@ -1,54 +1,40 @@
 const express = require('express');
 const next = require('next');
-const socketIO = require('socket.io');
+const socketIO = require('socket.io')
 const http = require('http');
-const { createSupabaseClient } = require('./src/auth/server');
 
 const dev = process.env.NODE_ENV !== 'production';
 const nextApp = next({ dev });
 const nextHandle = nextApp.getRequestHandler();
 
-const supabase = createSupabaseClient();
-
 nextApp.prepare().then(() => {
-  const app = express();
-  const server = http.createServer(app); // Create an HTTP server
-  const io = socketIO(server); // Attach socket.io to the HTTP server
+    const app = express();
+    const server = http.createServer(app); // Create an HTTP server
+    const io = socketIO(server); // Attach socket.io to the HTTP server
 
-  // Handle socket.io connection
-  io.on('connection', async (socket) => {
-    console.log('A client connected');
-    const { token } = socket.handshake.query;
+    // Handle socket.io connection
+    io.on('connection', (socket) => {
+        console.log('A client connected');
+        
 
-    const { data, error } = await supabase
-      .from('active_tokens')
-      .select('*')
-      .eq('token', token)
-      .single();
+        // Handle socket.io events here
 
-    // Disconnect the user if the token is invalid.
-    if (error || !data) {
-      console.log('Invalid token');
-      socket.disconnect();
-      return;
-    }
-
-    // Handle socket.io events here
-
-    socket.on('disconnect', () => {
-      console.log('A client disconnected');
+        // Handle socket.io disconnection
+        socket.on('disconnect', () => {
+            console.log('A client disconnected');
+        });
     });
-  });
 
-  app.all('*', (req, res) => {
-    return nextHandle(req, res);
-  });
+    app.all('*', (req, res) => {
+        return nextHandle(req, res);
+    });
 
-  server.listen(3000, (err) => {
-    if (err) throw err;
-    console.log('> Ready on http://localhost:3000');
-  });
+    server.listen(3000, (err) => {
+        if (err) throw err;
+        console.log('> Ready on http://localhost:3000');
+    });
+
 }).catch((ex) => {
-  console.error(ex.stack);
-  process.exit(1);
+    console.error(ex.stack)
+    process.exit(1)
 });
