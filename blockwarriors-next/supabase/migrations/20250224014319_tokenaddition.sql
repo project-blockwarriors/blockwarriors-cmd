39d19b9ff913ee387dc36049e2224422a8dca7f6
DIodide <ibraheem.amin3@gmail.com>
2025-02-24 20:48:01 -0500

    update migration refactor

--- blockwarriors-next/supabase/migrations/20250224014319_tokenaddition.sql
+++ blockwarriors-next/supabase/migrations/20250224014319_tokenaddition.sql
@@ -3,34 +3,34 @@
 -- once all players are logged in to the server (they will be in a waiting state prior)
 -- and all teams are full, the match status will be set to "starting" -> "completed".
 
-CREATE TABLE dev_active_tokens (
+
+
+CREATE TABLE active_tokens (
     token UUID NOT NULL DEFAULT uuid_generate_v4(), -- uuidv4
     user_id UUID REFERENCES public.users(user_id) ON DELETE CASCADE,
     bot_id INTEGER GENERATED ALWAYS AS IDENTITY,
-    match_id BIGINT REFERENCES dev_matches(match_id) ON DELETE CASCADE,
-    team_id BIGINT REFERENCES dev_practice_teams(team_id) ON DELETE CASCADE,
+    match_id BIGINT REFERENCES matches(match_id) ON DELETE CASCADE,
+    game_team_id BIGINT REFERENCES game_teams(team_id) ON DELETE CASCADE,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
     expires_at TIMESTAMP WITH TIME ZONE DEFAULT (NOW() + INTERVAL '15 minutes'),
     CONSTRAINT active_tokens_pkey PRIMARY KEY (token)
 );
 
-CREATE TABLE dev_matches (
+CREATE TABLE matches (
     match_id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY, -- auto increment
-    match_type TEXT NOT NULL REFERENCES dev_practice_teams(match_type),
+    match_type TEXT NOT NULL,
     match_status TEXT DEFAULT 'pending',
-    match_elo INTEGER,
-    red_team_id BIGINT REFERENCES dev_practice_teams(team_id),
-    blue_team_id BIGINT REFERENCES dev_practice_teams(team_id),
+    red_team_id BIGINT REFERENCES game_teams(team_id),
+    blue_team_id BIGINT REFERENCES game_teams(team_id),
     created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
     expires_at TIMESTAMP WITH TIME ZONE DEFAULT (NOW() + INTERVAL '15 minutes')
 );
 
-CREATE TABLE dev_practice_teams (
-    team_id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    team_name TEXT NOT NULL,
-    match_type TEXT NOT NULL,
-    team_leader_id UUID REFERENCES public.users(user_id) ON DELETE SET NULL,
-    total_team_elo INTEGER DEFAULT 0,
+CREATE TABLE game_teams (
+    game_team_id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    -- red bots is a dynamic range postgres array
+    red_bots INTEGER[] NOT NULL,
+    blue_bots INTEGER[] NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
 );
 
@@ -39,11 +39,11 @@ CREATE OR REPLACE FUNCTION delete_expired_records()
 RETURNS VOID AS $$
 BEGIN
     -- Delete expired tokens
-    DELETE FROM dev_active_tokens
+    DELETE FROM active_tokens
     WHERE expires_at < NOW();
 
     -- Delete matches that have expired and are still pending
-    DELETE FROM dev_matches
+    DELETE FROM matches
     WHERE expires_at < NOW() AND status = 'pending';
 END;
 $$ LANGUAGE plpgsql;
